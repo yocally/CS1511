290cb61700f545e4c3de5da861efda1a11aa060e
Cally <themodempress@gmail.com>
2016-10-01 20:42:33 -0500

    Modified project4.cpp added computeDifferense function and started with computing the minutes

--- project4.cpp
+++ project4.cpp
@@ -13,6 +13,17 @@ namespace jump {
 }
 
 
+int computeDifference(int sHours, int sMinutes, bool sIsAM, int jHours, int jMinutes, bool jIsAM) {
+    int result;
+    if (sMinutes < jMinutes) {
+        result = jMinutes - sMinutes;
+    }
+    else {
+        // how?
+    }
+
+    return result;
+}
 int main() {
     cout << "Please enter the time you are starting at:\nHours: ";
     cin >> start::hours;
@@ -29,5 +40,6 @@ int main() {
     cout << "Is it AM(y/n): ";
     cin >> ans;
     if (ans == 'y') { jump::isAM = true; } else { jump::isAM = false; }
-    
+
+    cout << "Differense in minutes: " << computeDifference(start::hours, start::minutes, start::isAM, jump::hours, jump::minutes, jump::isAM);
 }
