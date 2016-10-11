4840bbb8303746dfa0a155e572c5f8e7682d649d
Cally <themodempress@gmail.com>
2016-10-11 01:39:00 -0500

    rewrote entire program, made computeDifference break everything down in to minutes and figure the difference between those

--- project4.cpp
+++ project4.cpp
@@ -1,120 +1,64 @@
 /**
     Project 4
-    @version 1.0
+    @version 2.0
     @author Cally Sibben
 
     @description
-    Given a start time and an end time, assuming they are within 24 hours
-    of eachother, it will calculate the ammount of time between start
-    and finish. Something about a time machine? I don't know
-
-    @notes
-    Oh my god I could do this whole thing with for loops and it would be
-    REALLY FUCKIN' JANKY but it'd work. Don't do that
-    start:
-    hours:9
-    minutes:30
-    isAM:n
-
-    jump:
-    hours:3
-    minutes:15
-    isAM:y
-    ^^This doesn't work
-
-    FINISHED:
-    The minutes calculation is fully functional
-    The hours calculation is fully functional
-    AM PM check
+    Time machine distance calculation
 */
 
 #include <iostream>
 
 using namespace std;
 
-class timeHolder {
-public:
-    int hours;
-    int minutes;
-    bool isAM;
-};
 
-timeHolder start;
-timeHolder jump;
-
-char ans;
-
-
-int computeDifference(int sHours, int sMinutes, bool sIsAM, int jHours, int jMinutes, bool jIsAM) {
-    int result;
-
-    // Check and compute the minutes
-    // Minutes should be working flawlessly, except that when given 0 and 0
-    // it thinks that it's a full hour
-    if ((sMinutes == 0) && (jMinutes == 0)) {
-        // Place holder so that nothing happens if they're both 0
-    }
-    else if (sMinutes < jMinutes) {
-        // If the start minutes are less than the jump minutes, you can
-        // just do straight math to get the result
-        result = jMinutes - sMinutes;
-    }
-    else {
-        // In the case that the start minutes are greather than the jump
-        // minutes, I do not remember how this works
-        result = (60 - sMinutes) + jMinutes;
-    }
-
-    cout << "Post minutes calculation: " << result << "\n";
-
-    // Check and compute the hours
-    if ((sHours == 0) && (jHours == 0)) {
-        // Place holder so that nothing happens if they're both 0
-    }
-    else if (sHours < jHours) {
-        // If the start minutes are less than the jump minutes, you can
-        // just do straight math to get the result
-        result += (jHours - sHours);
-    }
-    else {
-        // In the case that the start minutes are greater than the jump
-        // minutes, I do not remember how this works
-        result += ((12 - sHours) + jHours) * 60;
-    }
-
-    cout << "Post hours calculation: " << result << "\n";
-
-
-    // Account for AM/PM
-    if (jIsAM == sIsAM) {
-        // if they're the same, no biggie
-    }
-    else {
-        // Because it doesn't matter if we're going am to pm, or
-        // pm to am, we're still jumping ahead 12 hours
-        // result += 12*60;
-    }
-    return result;
-}
-
+void readTime(int& m, int& h, bool& is);
+int computeDifference(int sHours, int jHours, int sMinutes, int jMinutes, bool sIsAM, bool jIsAM);
 
 int main() {
-    cout << "Please enter the time you are starting at:\nHours: ";
-    cin >> start.hours;
-    cout << "Minutes: ";
-    cin >> start.minutes;
-    cout << "Is it AM(y/n): ";
-    cin >> ans;
-    if (ans == 'y') { start.isAM = true; } else { start.isAM = false; }
+    int hours, hours2, minutes, minutes2;
+    bool isAM, isAM2;
 
-    cout << "\nPlease enter the time you are jumping too:\nHours: ";
-    cin >> jump.hours;
-    cout << "Minutes: ";
-    cin >> jump.minutes;
-    cout << "Is it AM(y/n): ";
-    cin >> ans;
-    if (ans == 'y') { jump.isAM = true; } else { jump.isAM = false; }
+    cout << "Enter the start time in format HH:MM (AM/PM): ";
+    readTime(minutes, hours, isAM);
 
-    cout << "Differense in minutes: " <<
-    computeDifference(start.hours, start.minutes, start.isAM, jump.hours, jump.minutes, jump.isAM);
+    cout << "Enter the jump time in format HH:MM (AM/PM): ";
+    readTime(minutes2, hours2, isAM2);
+
+    cout << "The result is " << computeDifference(hours, hours2, minutes, minutes2, isAM, isAM2) << " minutes\n";
+}
+
+void readTime(int& m, int& h, bool& is) {
+    char extra;
+    char ap;
+    cin >> h >> extra >> m >> ap >> extra;
+    if(ap == 'a') {
+        is = true;
+    }
+    else {
+        is = false;
+    }
+
+}
+
+int computeDifference (int sHours, int jHours, int sMinutes, int jMinutes, bool sIsAM, bool jIsAM) {
+    int start, jump;
+
+    start = sMinutes;
+    start += sHours*60;
+    if (!sIsAM) { start += 12*60; }
+
+    cout << "Start is: " << start << "\n";
+    jump = jMinutes;
+    jump += jHours*60;
+    if (!jIsAM) { jump += 12*60; }
+
+    cout << "Jump is: " << jump << "\n";
+
+    if(start < jump) {
+        return (jump - start);
+    }
+    else {
+        return ((1440 - start) + jump);
+    }
 }
