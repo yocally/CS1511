d38148842582afb6b90c5d46520b8333f760a1b6
Cally <themodempress@gmail.com>
2016-10-18 14:30:03 -0500

    Started adding the split routine (it doesn't work worth shit) updated gitignore added babynames.txt

--- project5.cpp
+++ project5.cpp
@@ -9,12 +9,48 @@
     of the name, then the male and then the female for that rank. The program
     should then be able to take user input for a name, then print out the ranking
     for both the male and female for that name
+
+    @notes
+    So aparently there is no split string function, and that's pretty terrible. If it was up to
+    me, I'd call a python file just for the split function.
 */
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
-int main() {
+string rank[1000];
+string girlnames[1000];
+string boynames[1000];
 
+string split(string srcString, char delimeter, )
+int main() {
+    fstream infile;
+    string line;
+    infile.open("babynames.txt");
+
+    int i = 0;
+    while(getline(infile, line))
+    {
+        int x = 0;
+        while (line[x] != ' ')
+        {
+            rank[i] += line[x];
+            x++;
+        }
+        x++;
+        while (line[x] != ' ')
+        {
+            boynames[i] += line[x];
+        }
+        x++;
+        while (line[x] != ' ')
+        {
+            girlnames[i] += line[x];
+        }
+        i++;
+    }
+    cout << rank[149] << " " << boynames[149] << " " << girlnames[149];
 }
